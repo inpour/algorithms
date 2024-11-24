23eadae9c5dcf1a5fa119e78b85ff0c992021262
Amirhossein Aliakbarpour <ah.aliakbarpour@gmail.com>
2024-11-24 11:20:59 +0330

    Heap sort

---
+++ sort/heap.go
@@ -0,0 +1,37 @@
+package sort
+
+// Heap takes ~Nlog(N) time to sort any array of length N (assuming comparisons take constant time).
+// It makes at most 2N*log(N) compares.
+// This sorting algorithm is not stable.
+// It uses ~1 extra memory (not including the input array).
+// The complexity is O(N*log(N)) where N = len(x).
+func Heap[T any](x []T, less func(a, b T) bool) {
+	n := len(x)
+
+	// heapify
+	for k := n / 2; k >= 1; k-- {
+		sink(x, k, n, less)
+	}
+
+	// sort-down
+	k := n
+	for k > 1 {
+		x[0], x[k-1] = x[k-1], x[0]
+		k--
+		sink(x, 1, k, less)
+	}
+}
+
+func sink[T any](x []T, k, n int, less func(a, b T) bool) {
+	for 2*k <= n {
+		j := 2 * k
+		if j < n && less(x[j-1], x[j]) {
+			j++
+		}
+		if !less(x[k-1], x[j-1]) {
+			break
+		}
+		x[k-1], x[j-1] = x[j-1], x[k-1]
+		k = j
+	}
+}
