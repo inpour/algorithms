bfbe304ae436a10740841dffa11a4d34161eb56a
Amirhossein Aliakbarpour <ah.aliakbarpour@gmail.com>
2024-12-21 13:58:26 +0330

    Update symbol table interfaces

--- search/symbol_table.go
+++ search/symbol_table.go
@@ -7,8 +7,8 @@ import (
 
 type SymbolTable[K, V any] interface {
 	Put(key K, val V)          // put key-value pair into the table (overwrite value if key already exists)
-	Get(key K) (V, error)      // get value paired with key, ErrAbsentKey error if key is absent
-	Delete(key K) error        // remove key (and its value) from table, ErrAbsentKey error if key is absent
+	Get(key K) (V, error)      // get value paired with key, ErrAbsentKey if key is absent
+	Delete(key K) error        // remove key and associated value, ErrAbsentKey if key is absent
 	Contains(key K) bool       // is there a value paired with key?
 	IsEmpty() bool             // is the table empty?
 	Size() int                 // number of key-value pairs in the table
@@ -17,17 +17,20 @@ type SymbolTable[K, V any] interface {
 
 type OrderedSymbolTable[K, V any] interface {
 	SymbolTable[K, V]
-	Min() K                                 // smallest key
-	max() K                                 // largest key
-	Floor(key K) (K, error)                 // largest key less than or equal to key, ErrAbsentKey error if key is absent
-	Ceiling(key K) (K, error)               // smallest key greater than or equal to key, ErrAbsentKey error if key is absent
-	Rank(key K) (int, error)                // number of keys less than key, ErrAbsentKey error if key is absent
-	Select(k int) (K, error)                // key of rank k, ErrInvalidRank error if rank is out of range
-	DelMin()                                // delete smallest key
-	DelMax()                                // delete largest key
-	RangeSize(lo, hi K) int                 // number of keys in [lo:hi]
-	RangeIterator(lo, hi K) iter.Seq2[K, V] // key-value pairs where keys in [lo:hi], in sorted order
+	Min() (K, error)                        // smallest key, ErrEmptySymbolTable if symbol table is empty
+	Max() (K, error)                        // largest key, ErrEmptySymbolTable if symbol table is empty
+	Floor(key K) (K, error)                 // largest key less than or equal to key, ErrTooSmallFloorKey if key to floor is too small
+	Ceiling(key K) (K, error)               // smallest key greater than or equal to key, ErrTooLargeCeilingKey if key to ceiling is too large
+	Rank(key K) (int, error)                // number of keys less than key, ErrAbsentKey if key is absent
+	Select(k int) (K, error)                // key of rank k, ErrInvalidRank if rank is out of range
+	DelMin() error                          // delete the smallest key and associated value, ErrEmptySymbolTable if symbol table is empty
+	DelMax() error                          // delete the largest key and associated value, ErrEmptySymbolTable if symbol table is empty
+	RangeSize(lo, hi K) int                 // number of keys in [lo:hi] range
+	RangeIterator(lo, hi K) iter.Seq2[K, V] // key-value pairs where keys in [lo:hi] range, in sorted order
 }
 
 var ErrAbsentKey = errors.New("key is absent")
 var ErrInvalidRank = errors.New("rank is out of range")
+var ErrEmptySymbolTable = errors.New("symbol table is empty")
+var ErrTooSmallFloorKey = errors.New("key to floor is too small")
+var ErrTooLargeCeilingKey = errors.New("key to ceiling is too large")
