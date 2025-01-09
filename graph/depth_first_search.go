7aa81432411be619b40eb37d911fd89952af9c67
Amirhossein Aliakbarpour <ah.aliakbarpour@gmail.com>
2025-01-09 15:49:39 +0330

    DepthFirstSearch multiple-source reachability

--- graph/depth_first_search.go
+++ graph/depth_first_search.go
@@ -1,6 +1,6 @@
 package graph
 
-// DepthFirstSearch (DFS) represents a data type for determining the vertices connected to a given source vertex (s) in a graph.
+// DepthFirstSearch (DFS) represents a data type for determining single-source or multiple-source reachability in a graph.
 // It uses O(V) extra space (not including the graph), where V is the number of vertices.
 type DepthFirstSearch struct {
 	marked []bool // marked[v] = is there an s-v path?
@@ -18,6 +18,26 @@ func NewDepthFirstSearch(graph UndirectedOrDirectedGraph, s int) (*DepthFirstSea
 		count:  0,
 	}
 	d.dfs(graph, s)
+	return d, nil
+}
+
+// NewDepthFirstSearchMultiSource computes the vertices in graph that are connected to any of the source vertices (sources).
+// The complexity is O(V + E), where V is the number of vertices and E is the number of edges.
+func NewDepthFirstSearchMultiSource(graph UndirectedOrDirectedGraph, sources []int) (*DepthFirstSearch, error) {
+	for _, s := range sources {
+		if err := graph.validateVertex(s); err != nil {
+			return nil, err
+		}
+	}
+	d := &DepthFirstSearch{
+		marked: make([]bool, graph.V()),
+		count:  0,
+	}
+	for _, s := range sources {
+		if !d.marked[s] {
+			d.dfs(graph, s)
+		}
+	}
 	return d, nil
 }
 
@@ -42,7 +62,7 @@ func (d *DepthFirstSearch) Marked(v int) (bool, error) {
 	return d.marked[v], nil
 }
 
-// Count returns the number of vertices connected to the source vertex (s).
+// Count returns the number of vertices reachable from the source vertex or source vertices.
 // The complexity is O(1).
 func (d *DepthFirstSearch) Count() int {
 	return d.count
